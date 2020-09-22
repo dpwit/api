df4ac45dd3ad439a999bfd0c52d3f0a403b2dc36
Darren Whatford <darren.whatford@dpwit.co.uk>
2020-09-22 09:54:20 +0100

    Added div container and img  to root app

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,9 @@ const app = document.getElementById('root')
 
 const logo = document.createElement('img')
 logo.src = 'img/DPW_Skull_Logo_Mask.png'
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+app.appendChild(logo)
+app.appendChild(container)
