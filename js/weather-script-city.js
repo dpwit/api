e40b014535887becd3f1b092d0de6ca524afcd40
Darren Whatford <darren.whatford@dpwit.co.uk>
2020-09-25 16:52:42 +0100

    Added new js file update html file

---
+++ js/weather-script-city.js
@@ -0,0 +1,42 @@
+const key = '24d10e4fb421418e6e7fc7ee3e891b4c';
+
+function weatherBallon( cityID ) {
+	fetch('https://api.openweathermap.org/data/2.5/weather?q=London' + '&appid=' + key)
+	//fetch('http://bulk.openweathermap.org/snapshot/weather_14.json.gz?' + '&appid=' + key)
+
+	.then(function(resp) { return resp.json() }) // Convert data to json
+	.then(function(data) {
+		drawWeather(data);
+	})
+	.catch(function() {
+		// catch any errors
+	});
+}
+function drawWeather( d ) {
+  var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
+  var description = d.weather[0].description; 
+  var weatherMain = d.weather[0].main;
+  var humidity = d.main.temp;
+  var windSpeed = d.wind.speed;
+	
+	document.getElementById('description').innerHTML = description;
+	document.getElementById('temp').innerHTML = celcius + '&deg;';
+	document.getElementById('location').innerHTML = d.name;
+	document.getElementById('humidity').innerHTML = humidity;
+	document.getElementById('windSpeed').innerHTML = windSpeed;
+	document.getElementById('main').innerHTML = weatherMain;
+  
+  if( description.indexOf('rain') > 0 ) {
+  	document.body.className = 'rainy';
+  } else if( description.indexOf('cloud') > 0 ) {
+  	document.body.className = 'cloudy';
+  } else if( description.indexOf('sunny') > 0 ) {
+  	document.body.className = 'sunny';
+  } else {
+  	document.body.className = 'clear';
+  }
+}
+window.onload = function() {
+	weatherBallon( 703448 );
+}
