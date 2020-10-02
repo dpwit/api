71c7cfcec1955b262f11725d287dbebe795f2477
darrenwhatford <darrenwhatford@hastingsdirect.com>
2020-10-02 16:23:19 +0100

    Updated code in js file

--- js/weather-script-city.js
+++ js/weather-script-city.js
@@ -1,42 +1,41 @@
 const key = '24d10e4fb421418e6e7fc7ee3e891b4c';
 
-function weatherBallon( cityID ) {
-	fetch('https://api.openweathermap.org/data/2.5/weather?q=London' + '&appid=' + key)
-	//fetch('http://bulk.openweathermap.org/snapshot/weather_14.json.gz?' + '&appid=' + key)
+function weatherData() {
+	fetch('https://api.openweathermap.org/data/2.5/weather?q=Rome' + '&appid=' + key)
 
 	.then(function(resp) { return resp.json() }) // Convert data to json
 	.then(function(data) {
-		drawWeather(data);
+		getWeatherData(data);
 	})
 	.catch(function() {
 		// catch any errors
 	});
 }
-function drawWeather( d ) {
-  var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+function getWeatherData( d ) {
+	var celcius = Math.round(parseFloat(d.main.temp)-273.15);
 	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
-  var description = d.weather[0].description; 
-  var weatherMain = d.weather[0].main;
-  var humidity = d.main.temp;
-  var windSpeed = d.wind.speed;
-	
+	var description = d.weather[0].description;
+	var weatherMain = d.weather[0].main;
+	var humidity = d.main.temp;
+	var windSpeed = d.wind.speed;
+
 	document.getElementById('description').innerHTML = description;
 	document.getElementById('temp').innerHTML = celcius + '&deg;';
 	document.getElementById('location').innerHTML = d.name;
 	document.getElementById('humidity').innerHTML = humidity;
 	document.getElementById('windSpeed').innerHTML = windSpeed;
 	document.getElementById('main').innerHTML = weatherMain;
-  
-  if( description.indexOf('rain') > 0 ) {
-  	document.body.className = 'rainy';
-  } else if( description.indexOf('cloud') > 0 ) {
-  	document.body.className = 'cloudy';
-  } else if( description.indexOf('sunny') > 0 ) {
-  	document.body.className = 'sunny';
-  } else {
-  	document.body.className = 'clear';
-  }
+
+	if( description.indexOf('rain') > 0 ) {
+		document.body.className = 'rainy';
+	} else if( description.indexOf('cloud') > 0 ) {
+		document.body.className = 'cloudy';
+ 	} else if( description.indexOf('sunny') > 0 ) {
+		document.body.className = 'sunny';
+	} else {
+		document.body.className = 'clear';
+	}
 }
 window.onload = function() {
-	weatherBallon( 703448 );
+	weatherData();
 }
